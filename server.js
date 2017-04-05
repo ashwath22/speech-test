36895d3d5c5305eed6b3199cee1d9f8950d4e091
ashwath22 <ashwath.anand.22@gmail.com>
2017-04-05 12:00:20 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// class 14
+// http is a library like handlebars, firebase, etc. require brings it in.
+var express = require('express');
+var app = express();
+
+var router = express.Router();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+router.get('/', function(req, res) {
+	res.render('home');
+})
+
+app.use('/', router);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
